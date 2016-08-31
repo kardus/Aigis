04bfdde13acfff66d4573966d8354b5d7a56392b
jacob zlogar <jacob.zlogar@gmail.com>
2016-08-31 17:14:35 -0400

    aigis

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ var email = process.env.EMAIL;
 var password = process.env.PASSWORD;
 var client = new Discord.Client();
 
-client.loginWithToken(token, email, password, output);
+client.loginWithToken(token, output);
 
 function output(error, token) {
-        if (error) {
-                console.log('There was an error logging in: ' + error);
-                return;
-        } else
-                console.log('Logged in. Token: ' + token);
+  if (error) {
+    console.log('There was an error logging in: ' + error);
+    return;
+  } else {
+      console.log('Logged in. Token: ' + token);
+  }
 }
+
+client.on('message', function(message) {
+  if (message.channel.isPrivate) {
+    console.log('(Private) ${message.author.name}: ${message.content}');
+  } else {
+      console.log('(${message.server.name} / ${message.channel.name}) ${message.author.name}: ${message.content}');
+  }
+});
